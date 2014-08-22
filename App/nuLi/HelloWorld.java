208990d5d11430cd057552d4edf473ff0ab9f8fc
Kevin Simonson <simonsonkm@familysearch.org>
2014-08-22 13:37:06 -0600

    Stripped out line numbers.

--- App/nuLi/HelloWorld.java
+++ App/nuLi/HelloWorld.java
@@ -1,11 +1,11 @@
-import java.io.PrintStream;                       //: 1
-                                                  //: 2
-public class HelloWorld                           //: 3
-{                                                 //: 4
+import java.io.PrintStream;
+
+public class HelloWorld
+{
   private static final PrintStream SY_O = System.out;
-                                                  //: 6
-  public static void main ( String[] arguments)   //: 7
-  {                                               //: 8
-    SY_O.println( "Hello world!");                //: 9
-  }                                              //: 10
-}                                                //: 11
+
+  public static void main ( String[] arguments)
+  {
+    SY_O.println( "Hello world!");
+  }
+}
